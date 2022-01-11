28c5f9d9f15f3182af799cda5a2ef6721b404f62
Zantana Kahsay <91621786+KahsayZantana@users.noreply.github.com>
2022-01-10 20:57:21 -0500

    Moves JS from bottom of page to main.js

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@
     
  //manages mobile nav 
  function myFunction() {
- var x = document.getElementById("myTopnav");
- if (x.className === "topnav") {
- x.className += " responsive";
- } else {
- x.className = "topnav";
- }
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+      x.className += " responsive";
+   } else {
+      x.className = "topnav";
+  }
  }   
